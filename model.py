1c51e837f9fadd01769a3a91c53621865d31c7ff
Irfanullah <sahibzada.iu@gmail.com>
2024-10-08 01:47:00 +0900

    Update model.py

--- model.py
+++ model.py
@@ -16,20 +16,3 @@ class GCN(nn.Module):
         h_dst = h[:mfgs[1].num_dst_nodes()]
         h = self.conv2(mfgs[1], (h, h_dst))
         return h
-
-class SAGE(nn.Module):
-    def __init__(self, in_feats, h_feats, num_classes):
-        super(SAGE, self).__init__()
-        self.conv1 = SAGEConv(in_feats, h_feats, aggregator_type='mean')
-        self.conv2 = SAGEConv(h_feats, num_classes, aggregator_type='mean')
-        self.h_feats = h_feats
-
-    def forward(self, mfgs, x):
-        # Lines that are changed are marked with an arrow: "<---"
-
-        h_dst = x[:mfgs[0].num_dst_nodes()]  # <---
-        h = self.conv1(mfgs[0], (x, h_dst))  # <---
-        h = F.relu(h)
-        h_dst = h[:mfgs[1].num_dst_nodes()]  # <---
-        h = self.conv2(mfgs[1], (h, h_dst))  # <---
-        return h
